64d9a699f8ed0c7e4fab602269a3881aef4f4e98
Karolis Narkevicius <karolis.n@gmail.com>
2023-11-05 14:20:44 +0000

    Updates

--- _next/static/6Lx_lWL0zA6cQhxfL8lbA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/19-54c1a6035e91e184.js","static/chunks/pages/index-d5b9db24f9e223b9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/HAED1Xpfp-XNkwcF7AR44/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/19-a909afd223e4279d.js","static/chunks/pages/index-dd220ed2df8325c1.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
