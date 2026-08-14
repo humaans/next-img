0f6ae6caa73b8528fee2fe69a25e9901151d3ae7
Karolis Narkevicius <karolis.n@gmail.com>
2026-08-14 17:38:27 +0100

    Fix CLI build with Next.js 16

---
+++ test/next-build.test.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
+const test = require('ava')
+const createNextBuild = require('../lib/next-build')
+
+function createNextFixture(t, { bundler } = {}) {
+  const base = fs.mkdtempSync(path.join(os.tmpdir(), 'next-img-'))
+  const dist = path.join(base, 'dist')
+
+  fs.mkdirSync(path.join(dist, 'lib'), { recursive: true })
+  fs.writeFileSync(path.join(dist, 'build.js'), 'module.exports.default = (...args) => args')
+
+  if (bundler) {
+    fs.writeFileSync(path.join(dist, 'lib/bundler.js'), `exports.Bundler = ${JSON.stringify(bundler)}`)
+  }
+
+  t.teardown(() => fs.rmSync(base, { recursive: true, force: true }))
+  return base
+}
+
+test('selects webpack when Next.js exposes its bundler API', t => {
+  const base = createNextFixture(t, { bundler: { Turbopack: 0, Webpack: 1 } })
+  const nextBuild = createNextBuild(base)
+
+  t.deepEqual(nextBuild('/app'), ['/app', undefined, undefined, undefined, undefined, undefined, undefined, 1])
+})
+
+test('supports Next.js versions without the bundler API', t => {
+  const base = createNextFixture(t)
+  const nextBuild = createNextBuild(base)
+
+  t.deepEqual(nextBuild('/app'), ['/app'])
+})
