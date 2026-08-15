fe57e520b35d009fe413f122b13fba0934ddb5fd
Karolis Narkevicius <karolis.n@gmail.com>
2026-08-15 15:50:57 +0100

    Update docs deps

--- docs/src/components/Layout.js
+++ docs/src/components/Layout.js
@@ -7,16 +7,14 @@ export function Layout({ children }) {
       <div className='header'>
         <div className='header-inner'>
           <div>
-            <Link href='/' legacyBehavior>
-              <a className='logo'>next-img</a>
+            <Link href='/' className='logo'>
+              next-img
             </Link>
           </div>
 
           <ul className='nav'>
             <li>
-              <Link href='https://github.com/humaans/next-img/' legacyBehavior>
-                <a>View on Github →</a>
-              </Link>
+              <a href='https://github.com/humaans/next-img/'>View on Github →</a>
             </li>
           </ul>
         </div>
